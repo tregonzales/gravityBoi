6387eeaeb8973259cdd150e29c3658eeff9a42ae
Tre Gonzales <Tre@education.com>
2018-02-11 17:07:14 +0100

    a ton of sprites that we should delete after i flood the repo w assets

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,42 +5,58 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
 
 	Rigidbody2D boi;
-	public float boostSpeed;
-	public float antiGravSpeed;
+	public float gravBoostSpeed;
+	public float antiGravBoostSpeed;
 	public float gravScale;
 	public float numBoosts;
 	private float curSpeed;
 	// Use this for initialization
 	void Start () {
 		boi = GetComponent<Rigidbody2D>();
-		curSpeed = antiGravSpeed;
+		curSpeed = antiGravBoostSpeed;
 	}
 	
 	
 	// Update is called once per frame
 	void Update () {
 
+		//toggle gravity and change the speed of boost depending on current gravity 
 		 if (Input.GetKeyDown(KeyCode.Space)){
 			 if(boi.gravityScale == 0) {
 				 boi.gravityScale =gravScale;
-				 curSpeed = boostSpeed;
+				 curSpeed = gravBoostSpeed;
 			 }
 			 else {
             	boi.gravityScale = 0;
-				curSpeed = antiGravSpeed;
+				curSpeed = antiGravBoostSpeed;
 			 }
 		 }
 
+		//get the mouse position and use as directional vector to boost
 		Vector2 origin = transform.position;
 		Vector2 boost = Camera.main.ScreenToWorldPoint (Input.mousePosition);
 		Vector2 direction = (boost - origin).normalized;
+		OrientRobot(direction);
 		
+		//use the boost with left click, also reduce how many boosts the character has this level
 		if (Input.GetMouseButtonDown(0)) {
 			if (numBoosts != 0) {
 				boi.velocity = curSpeed*direction;
 				numBoosts--;
 			}
 		}
-        
+	}
+
+	void OrientRobot(Vector2 direction){
+
+		if (direction.x == 0 && direction.y == 0) {
+			//ugly but less code than two or statements bc im lazy rn tbh
+		}
+		else {
+			float angle = Mathf.Atan2(direction.y, direction.x);
+			angle = angle - Mathf.PI * 0.5f;
+			angle = angle * Mathf.Rad2Deg;
+			transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+		}
 	}
 }
