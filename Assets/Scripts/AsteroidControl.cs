029b6fa6f9dd9d28e986a9d3768c5a01e216f3f0
phetzler <phetzler@u.rochester.edu>
2018-02-14 17:03:39 +0100

    Added some asteroids

---
+++ Assets/Scripts/AsteroidControl.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidControl : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		//nothing
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	//sense collision and destroy if player
+}
---
+++ Assets/Scripts/AsteroidGenerator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidGenerator : MonoBehaviour {
+	public int numAsteroids, maxXVelocity, maxYVelocity;
+	public float minSize, maxSize;
+	public GameObject[] asteroids;
+	// Use this for initialization
+	void Start () {
+		for(int i = 0; i < numAsteroids; i++){
+			//Random number to select asteroid
+			//instantiate
+			//randomly scale x any y evenly
+			//add random velocity
+		}
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//do nothing
+	}
+}
