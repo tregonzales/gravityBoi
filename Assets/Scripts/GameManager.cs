2fed9fdf2b698a5e72a06f2c2d7b370a4fe5c3a0
Tre Gonzales <Tre@education.com>
2018-02-19 12:59:48 +0100

    edit game manager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,36 +1,16 @@
-﻿// <copyright file="GameManager.cs" company="DIS Copenhagen">
-// Copyright (c) 2017 All Rights Reserved
-// </copyright>
-// <author>Benno Lueders</author>
-// <date>07/14/2017</date>
-
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-/// <summary>
-/// Almost every game project includes some kind of Game Manager class that handles the overall flow of the game, namely loading and transitioning through scenes.
-/// This class uses the singleton pattern to provide easy access to the outside through the public static instance variable.
-/// </summary>
 public class GameManager : MonoBehaviour {
 
-	// Varibale to store our game manager instance. We need this in order to use instance only functionality like StartCoroutine() for example
 	public static GameManager instance;
 
-	public string nextScene;
-
-	/// <summary>
-	/// Use Awake to initialize the instance with the active script instance from the current scene.
-	/// </summary>
 	void Awake(){
 		instance = this;
 	}
 
-	/// <summary>
-	/// Reloads the current scene with some delay in seconds.
-	/// </summary>
-	/// <param name="seconds">Seconds.</param>
 	public void RestartTheGameAfterSeconds(float seconds){
 		StartCoroutine (LoadSceneAfterSeconds (seconds, SceneManager.GetActiveScene ().name));
 	}
@@ -40,13 +20,18 @@ public class GameManager : MonoBehaviour {
 	}
 
 	public void LoadNextScene(float seconds) {
-		StartCoroutine (LoadSceneAfterSeconds (seconds, nextScene));
+		StartCoroutine (LoadSceneAfterSeconds (seconds, null));
 	}
 
-	// Coroutine to start the game again
 	IEnumerator LoadSceneAfterSeconds(float seconds, string sceneName){
 		yield return new WaitForSeconds (seconds);
-		SceneManager.LoadScene (sceneName);
+		if (sceneName == null) {
+			SceneManager.LoadScene (SceneManager.GetActiveScene().buildIndex + 1);
+		}
+		else {
+			SceneManager.LoadScene (sceneName);
+		}
+		
 	}
 }
 
