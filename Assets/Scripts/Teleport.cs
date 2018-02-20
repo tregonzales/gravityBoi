b213ff88480f6a4d327b8be8acd41087c4e8877c
jeremymoss <jmoss@macalester.edu>
2018-02-20 13:48:16 +0100

    Added 3rd Scene

--- Assets/Scripts/Teleport.cs
+++ Assets/Scripts/Teleport.cs
@@ -9,14 +9,19 @@ public class Teleport : MonoBehaviour {
     public GameObject redGate2;
     public GameObject yellowGate1;
     public GameObject yellowGate2;
+    public GameObject blueGate1;
+    public GameObject blueGate2;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    Rigidbody2D boi;
+
+
+    // Use this for initialization
+    void Start () {
+        boi = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update () {
 		
 	}
 
@@ -38,6 +43,17 @@ public class Teleport : MonoBehaviour {
         else if (other.CompareTag("yellow2"))
         {
             transform.position = new Vector2(yellowGate1.transform.position.x + .25f, yellowGate1.transform.position.y);
+        }
+
+        if (other.CompareTag("blue1"))
+        {
+            transform.position = new Vector2(blueGate2.transform.position.x, blueGate2.transform.position.y + 1f);
+            boi.velocity = -boi.velocity;
+        }
+        else if (other.CompareTag("blue2"))
+        {
+            transform.position = new Vector2(blueGate1.transform.position.x, blueGate1.transform.position.y + 1f);
+            boi.velocity = -boi.velocity;
         }
 
 
