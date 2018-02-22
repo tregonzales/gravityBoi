fdb4ba308fa16d8765f46d96f64d008a0180fa81
phetzler <phetzler@u.rochester.edu>
2018-02-22 12:45:42 +0100

    Edited Asteroid Generation

--- Assets/Scripts/AsteroidGenerator.cs
+++ Assets/Scripts/AsteroidGenerator.cs
@@ -4,27 +4,32 @@ using UnityEngine;
 using System;
 
 public class AsteroidGenerator : MonoBehaviour {
-	public int numAsteroids, maxXVelocity, maxYVelocity;
+//	public int numAsteroids, maxXVelocity, maxYVelocity;
+	public Vector3[] AsteroidVelo;
 	public float minSize, maxSize;
-	public GameObject[] asteroids;
+	public GameObject[] possibleAsteroids;
 	// Use this for initialization
+
 	void Start () {
-        System.Random rand = new System.Random();
-		for(int i = 0; i < numAsteroids; i++){
+       	///* 
+		System.Random rand = new System.Random();
+		for(int i = 0; i < AsteroidVelo.Length; i++){
             //Random number to select asteroid
-			int r = (int)rand.Next(0,asteroids.Length);
+			int r = (int)rand.Next(0,possibleAsteroids.Length);
 			//instantiate
-			GameObject o = (GameObject)Instantiate(asteroids[r]);
+			Debug.Log(r);
+			GameObject o = (GameObject)Instantiate(possibleAsteroids[r]);
 			//randomly scale x any y evenly
 			double size = rand.NextDouble()*(maxSize-minSize) + minSize;
-			o.transform.position = transform.position;
-			o.transform.localScale = new Vector3((float)size,(float)size,0);
-			//add random velocity
-			double xVelo = rand.NextDouble() * maxXVelocity * 2 - maxXVelocity;
-			double yVelo = rand.NextDouble() * maxYVelocity * 2 - maxYVelocity;
-			o.GetComponent<Rigidbody2D>().velocity = new Vector3((float)xVelo,(float)yVelo,0);
+			
+			o.transform.position = new Vector3(transform.position.x + AsteroidVelo[i].x,transform.position.y + AsteroidVelo[i].y,transform.position.z);
 
+			o.transform.localScale = new Vector3((float)size,(float)size,0);
+			AsteroidVelo[i].z = 0;
+			o.GetComponent<Rigidbody2D>().velocity = AsteroidVelo[i];
+			
 		}
+		//*/
 		
 	}
 	
