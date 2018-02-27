4150135ee15e06e3397884678e99db7ccee23178
phetzler <phetzler@u.rochester.edu>
2018-02-27 10:18:41 +0100

    Revert "Original Intro"
    This reverts commit fea380f636054301a99859560ab79554b3a3afa6.

--- Assets/Scripts/AsteroidGenerator.cs
+++ Assets/Scripts/AsteroidGenerator.cs
@@ -4,19 +4,37 @@ using UnityEngine;
 using System;
 
 public class AsteroidGenerator : MonoBehaviour {
-	public Vector3[] AsteroidVelo; //holds asteroid start velocities
-	public float minSize, maxSize; //holds max and min scale of asteroid
-	public GameObject[] possibleAsteroids; //holds possible asteroids
+//	public int numAsteroids, maxXVelocity, maxYVelocity;
+	public Vector3[] AsteroidVelo;
+	public float minSize, maxSize;
+	public GameObject[] possibleAsteroids;
+	// Use this for initialization
 
 	void Start () {
-		for(int i = 0; i < AsteroidVelo.Length; i++){ //goes through all asteroids that are gonna be created
-			int r = (int)UnityEngine.Random.Range(0.0f,(float)possibleAsteroids.Length); //gets random asteroid to use
-			GameObject o = (GameObject)Instantiate(possibleAsteroids[r]);//instantiates the asteroid
-			double size = (double)UnityEngine.Random.Range(0.0f,1.0f)*(maxSize-minSize) + minSize; //get a random scale size within limits
-			o.transform.position = new Vector3(transform.position.x + AsteroidVelo[i].x,transform.position.y + AsteroidVelo[i].y,transform.position.z); //set start position of asteroid so they don't collide
-			o.transform.localScale = new Vector3((float)size,(float)size,0); //scale the asteroid
-			AsteroidVelo[i].z = 0; //make sure z is set to zero since 2D
-			o.GetComponent<Rigidbody2D>().velocity = AsteroidVelo[i]; //give the asteroid its initial velocity
+       	///* 
+		//UnityEngine.Random rand = new UnityEngine.Random();
+		for(int i = 0; i < AsteroidVelo.Length; i++){
+			//Debug.Log(Time.time);
+            //Random number to select asteroid
+			int r = (int)UnityEngine.Random.Range(0.0f,(float)possibleAsteroids.Length);
+			//instantiate
+			GameObject o = (GameObject)Instantiate(possibleAsteroids[r]);
+			//randomly scale x any y evenly
+			double size = (double)UnityEngine.Random.Range(0.0f,1.0f)*(maxSize-minSize) + minSize;
+			
+			o.transform.position = new Vector3(transform.position.x + AsteroidVelo[i].x,transform.position.y + AsteroidVelo[i].y,transform.position.z);
+
+			o.transform.localScale = new Vector3((float)size,(float)size,0);
+			AsteroidVelo[i].z = 0;
+			o.GetComponent<Rigidbody2D>().velocity = AsteroidVelo[i];
+			
 		}
+		//*/
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//do nothing
 	}
 }
